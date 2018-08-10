07b199e0ef6b4f5e6db021451453446782050396
Travis CI <>
2018-08-10 03:09:11 +0000

    Deploy to GitHub Pages: 5e26a9f961976d745630d0b012f7e518bf64fc2d

---
+++ ci/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# enable error reporting to the console
+set -e
+
+rm -rf ./_site/
+# build site with jekyll, by default to `_site' folder
+bundle install
+bundle exec jekyll build
+
+find .
